d9a9ba9ad43c0fad246aea87f6ed257cfdc4b551
Shanay Murdock <murdock.shanay@gmail.com>
2025-01-04 15:27:37 -0500

    Add DDL for posts table

--- ddl.sql
+++ ddl.sql
@@ -10,3 +10,19 @@ CREATE TABLE topics (
     name VARCHAR(30) UNIQUE NOT NULL,
     description VARCHAR(500)
 );
+
+-- Create `posts` table
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    url VARCHAR(4000) DEFAULT NULL,
+    text_content TEXT DEFAULT NULL,
+    topic_id INT NOT NULL,
+    user_id INT DEFAULT NULL,
+    CONSTRAINT check_url_or_text CHECK (
+        (url IS NOT NULL and text_content IS NULL) OR
+        (url IS NULL and text_content IS NOT NULL)
+    ),
+    FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL
+);
